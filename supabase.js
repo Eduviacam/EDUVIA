a790a027962ffd294da73744d33ed8f5c3b696a0
EDUVIA <EDUVIACAM@gmail.com>
2026-09-08 02:58:31 +0100

    Initial EDUVIA website

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://epxvxjofvaejrucyuuvd.supabase.co";
+const SUPABASE_KEY = "sb_publishable_-sXGc7RppVtAgL-nho6QCA_IVk-dmU8";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
